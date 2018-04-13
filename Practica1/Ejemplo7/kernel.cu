30e1fd4d21efcdb9664636598dc0436822ab20a4
Usuario Local <usuario_local@localhost.localdomain>
2018-04-13 14:56:29 +0200

    Noise reduction con memoria compartida hecho. VAMOOOOOOOS

--- Practica1/Ejemplo7/kernel.cu
+++ Practica1/Ejemplo7/kernel.cu
@@ -19,15 +19,17 @@ void noiseReductionGPU(float *im, float * im_NR_out, int h, int width){
 
 	__shared__ float shared_block [BLOCK_SIZE+4][BLOCK_SIZE+4];
 	 
-		
+
+	shared_block[x+2][y+2] = im[i*width+j];
+
 	//LLenado de la memoria compartida
 	//Llenamos las de arriba
 	//SI está en la primera fila del bloque Y no es la primera fila de la foto
-	if(threadIdx.y == 0 && i != 0) {
+	if(threadIdx.y == 0 && i > 2) {
 		shared_block[threadIdx.x+2][threadIdx.y+1] = im[(i-1)*width+j];
 		shared_block[threadIdx.x+2][threadIdx.y  ] = im[(i-2)*width+j];
 		//LLenamos la esquina superior izquierda del bloque
-		if(threadIdx.x == 0){
+		if(threadIdx.x == 0 && j > 2){
 			shared_block[threadIdx.x  ][threadIdx.y  ] = im[(i-2)*width+(j-2)];
 			shared_block[threadIdx.x  ][threadIdx.y+1] = im[(i-1)*width+(j-2)];
 			shared_block[threadIdx.x+1][threadIdx.y  ] = im[(i-2)*width+(j-1)];
@@ -40,7 +42,7 @@ void noiseReductionGPU(float *im, float * im_NR_out, int h, int width){
 		shared_block[threadIdx.x+2][threadIdx.y+3] = im[(i+1)*width+j];
 		shared_block[threadIdx.x+2][threadIdx.y+4]   = im[(i+2)*width+j];
 		//Llenamos la esquina inferior derecha del bloque
-		if(threadIdx.x == BLOCK_SIZE-1){
+		if(threadIdx.x == BLOCK_SIZE-1 && j < width-2){
 			shared_block[threadIdx.x+3][threadIdx.y+3] = im [(i+1)*width+(j+1)];
 			shared_block[threadIdx.x+3][threadIdx.y+4] = im [(i+2)*width+(j+1)];
 			shared_block[threadIdx.x+4][threadIdx.y+3] = im [(i+1)*width+(j+2)];
@@ -50,11 +52,11 @@ void noiseReductionGPU(float *im, float * im_NR_out, int h, int width){
 	}
 	//Lateral Izquierdo
 	//Si es la primera columna del bloque Y no es la primera columna de la foto, esto es (j == 0)
-	if(threadIdx.x == 0 && j != 0 ){
+	if(threadIdx.x == 0 && j > 2 ){
 		shared_block[threadIdx.x+1][threadIdx.y+2] = im [i * width+ (j-1)];
 		shared_block[threadIdx.x  ][threadIdx.y+2] = im [i * width+ (j-2)];
 		//LLenamos la esquina inferior izquierda del bloque
-		if(threadIdx.y == BLOCK_SIZE -1){
+		if(threadIdx.y == BLOCK_SIZE -1 && i < h-2){
 			shared_block[threadIdx.x  ][threadIdx.y+4] = im [(i+2)*width+(j-2)];
 			shared_block[threadIdx.x  ][threadIdx.y+3] = im [(i+1)*width+(j-2)];
 			shared_block[threadIdx.x+1][threadIdx.y+4] = im [(i+2)*width+(j-1)];
@@ -67,29 +69,26 @@ void noiseReductionGPU(float *im, float * im_NR_out, int h, int width){
 		shared_block[threadIdx.x+3][threadIdx.y+2] = im [i*width +(j+1)];
 		shared_block[threadIdx.x+4][threadIdx.y+2] = im [i*width +(j+2)];
 		//Llenamos la esquina superior derecha del bloque
-		if(threadIdx.y == 0){
+		if(threadIdx.y == 0 && i > 2){
 			shared_block[threadIdx.x+3][threadIdx.y+1] = im[(i-1)*width + (j+1)];
-			shared_block[threadIdx.x+4][threadIdx.y+1] = im[(i-2)*width + (j+1)];
-			shared_block[threadIdx.x+3][threadIdx.y  ] = im[(i-1)*width + (j+2)];
-			shared_block[threadIdx.x+4][threadIdx.y  ] = im[(i-2)*width + (j+2)];		
+			shared_block[threadIdx.x+4][threadIdx.y+1] = im[(i-1)*width + (j+2)];
+			shared_block[threadIdx.x+3][threadIdx.y  ] = im[(i-2)*width + (j+1)];
+			shared_block[threadIdx.x+4][threadIdx.y  ] = im[(i-2)*width + (j+2)];	
 		}
 
 	}
 	
 	__syncthreads();
-	uint x = threadIdx.x;
-	uint y = threadIdx.y;
-
 	
-	if(i >= 2 && i < h-2 && j >= 2 && j < width-2)
+	if(i > 2 && i < h-2 && j >2 && j < width-2)
 	{
 		
 		im_NR_out[i*width+j] = 
-				 (2.0*shared_block[y  ][x  ] +  4.0*shared_block[y  ][x+1] +  5.0*shared_block[y  ][x+2] +  4.0*shared_block[y  ][x+3] + 2.0*shared_block[y  ][x+4]
-				+ 4.0*shared_block[y+1][x  ] +  9.0*shared_block[y+1][x+1] + 12.0*shared_block[y+1][x+2] +  9.0*shared_block[y+1][x+3] + 4.0*shared_block[y+1][x+4] 
-				+ 5.0*shared_block[y+2][x  ] + 12.0*shared_block[y+2][x+1] + 15.0*shared_block[y+2][x+2] + 12.0*shared_block[y+2][x+3] + 5.0*shared_block[y+2][x+4]
-				+ 4.0*shared_block[y+3][x  ] +  9.0*shared_block[y+3][x+1] + 12.0*shared_block[y+3][x+2] +  9.0*shared_block[y+3][x+3] + 4.0*shared_block[y+3][x+4]
-				+ 2.0*shared_block[y+4][x  ] +  4.0*shared_block[y+4][x+1] +  5.0*shared_block[y+4][x+2] +  4.0*shared_block[y+4][x+3] + 2.0*shared_block[y+4][x+4])
+				 (2.0*shared_block[x  ][y  ] +  4.0*shared_block[x+1][y  ] +  5.0*shared_block[x+2][y  ] +  4.0*shared_block[x+3][y  ] + 2.0*shared_block[x+4][y  ]
+				+ 4.0*shared_block[x  ][y+1] +  9.0*shared_block[x+1][y+1] + 12.0*shared_block[x+2][y+1] +  9.0*shared_block[x+3][y+1] + 4.0*shared_block[x+4][y+1] 
+				+ 5.0*shared_block[x  ][y+2] + 12.0*shared_block[x+1][y+2] + 15.0*shared_block[x+2][y+2] + 12.0*shared_block[x+3][y+2] + 5.0*shared_block[x+4][y+2]
+				+ 4.0*shared_block[x  ][y+3] +  9.0*shared_block[x+1][y+3] + 12.0*shared_block[x+2][y+3] +  9.0*shared_block[x+3][y+3] + 4.0*shared_block[x+4][y+3]
+				+ 2.0*shared_block[x  ][y+4] +  4.0*shared_block[x+1][y+4] +  5.0*shared_block[x+2][y+4] +  4.0*shared_block[x+3][y+4] + 2.0*shared_block[x+4][y+4])
 				/159.0;		
 	}
 	
