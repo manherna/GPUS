d9991e09ace69afa562bf9950e96457a3429d481
Usuario Local <usuario_local@localhost.localdomain>
2018-04-18 13:19:39 +0200

    AÑADIDA GRADIENTE :ok_hand:

--- Practica1/Ejemplo7/kernel.cu
+++ Practica1/Ejemplo7/kernel.cu
@@ -16,8 +16,8 @@ void noiseReductionGPU(float *im, float * im_NR_out, int h, int width){
 	uint y = threadIdx.y;
 
 	
-
-	__shared__ float shared_block [BLOCK_SIZE+4][BLOCK_SIZE+4];
+	//evitamos accesos coalescentes aumentando en 1 las columnas.
+	__shared__ float shared_block [BLOCK_SIZE+5][BLOCK_SIZE+4];
 	 
 
 	shared_block[x+2][y+2] = im[i*width+j];
@@ -79,7 +79,6 @@ void noiseReductionGPU(float *im, float * im_NR_out, int h, int width){
 	}
 	
 	__syncthreads();
-	
 	if(i > 2 && i < h-2 && j >2 && j < width-2)
 	{
 		
@@ -96,9 +95,110 @@ void noiseReductionGPU(float *im, float * im_NR_out, int h, int width){
 
 }
 
-__device__ 
-void imageGradientGPU (float *im_in, float * im_GR_out, int h, int w) {
+__global__ 
+void imageGradientGPU (float *im, float * im_out, int h, int width) {
+	
 
+
+	uint j = (blockIdx.x * blockDim.x) + threadIdx.x;
+	uint i = (blockIdx.y * blockDim.y) + threadIdx.y;
+	uint x = threadIdx.x;
+	uint y = threadIdx.y;
+
+	//Coalescencia o k ase
+	__shared__ float shared_block [BLOCK_SIZE+5][BLOCK_SIZE+4];
+	 
+	
+	//Shared block guarda los pixels de la imagen CON noise reduction
+	shared_block[x+2][y+2] = im[i*width+j];
+
+
+	//LLenado de la memoria compartida
+	//Llenamos las de arriba
+	//SI está en la primera fila del bloque Y no es la primera fila de la foto
+	if(threadIdx.y == 0 && i > 2) {
+		shared_block[threadIdx.x+2][threadIdx.y+1] = im[(i-1)*width+j];
+		shared_block[threadIdx.x+2][threadIdx.y  ] = im[(i-2)*width+j];
+		//LLenamos la esquina superior izquierda del bloque
+		if(threadIdx.x == 0 && j > 2){
+			shared_block[threadIdx.x  ][threadIdx.y  ] = im[(i-2)*width+(j-2)];
+			shared_block[threadIdx.x  ][threadIdx.y+1] = im[(i-1)*width+(j-2)];
+			shared_block[threadIdx.x+1][threadIdx.y  ] = im[(i-2)*width+(j-1)];
+			shared_block[threadIdx.x+1][threadIdx.y+1] = im[(i-1)*width+(j-1)];
+		}
+		
+	}
+	//Llenamos las de abajo
+	if(threadIdx.y == BLOCK_SIZE-1 && i < h-2){
+		shared_block[threadIdx.x+2][threadIdx.y+3] = im[(i+1)*width+j];
+		shared_block[threadIdx.x+2][threadIdx.y+4]   = im[(i+2)*width+j];
+		//Llenamos la esquina inferior derecha del bloque
+		if(threadIdx.x == BLOCK_SIZE-1 && j < width-2){
+			shared_block[threadIdx.x+3][threadIdx.y+3] = im [(i+1)*width+(j+1)];
+			shared_block[threadIdx.x+3][threadIdx.y+4] = im [(i+2)*width+(j+1)];
+			shared_block[threadIdx.x+4][threadIdx.y+3] = im [(i+1)*width+(j+2)];
+			shared_block[threadIdx.x+4][threadIdx.y+4] = im [(i+2)*width+(j+2)];		
+		}
+
+	}
+	//Lateral Izquierdo
+	//Si es la primera columna del bloque Y no es la primera columna de la foto, esto es (j == 0)
+	if(threadIdx.x == 0 && j > 2 ){
+		shared_block[threadIdx.x+1][threadIdx.y+2] = im [i * width+ (j-1)];
+		shared_block[threadIdx.x  ][threadIdx.y+2] = im [i * width+ (j-2)];
+		//LLenamos la esquina inferior izquierda del bloque
+		if(threadIdx.y == BLOCK_SIZE -1 && i < h-2){
+			shared_block[threadIdx.x  ][threadIdx.y+4] = im [(i+2)*width+(j-2)];
+			shared_block[threadIdx.x  ][threadIdx.y+3] = im [(i+1)*width+(j-2)];
+			shared_block[threadIdx.x+1][threadIdx.y+4] = im [(i+2)*width+(j-1)];
+			shared_block[threadIdx.x+1][threadIdx.y+3] = im [(i+1)*width+(j-1)];
+		}
+	
+	}
+	//Lateral derecho
+	if(threadIdx.x == BLOCK_SIZE-1 && j < width-2){
+		shared_block[threadIdx.x+3][threadIdx.y+2] = im [i*width +(j+1)];
+		shared_block[threadIdx.x+4][threadIdx.y+2] = im [i*width +(j+2)];
+		//Llenamos la esquina superior derecha del bloque
+		if(threadIdx.y == 0 && i > 2){
+			shared_block[threadIdx.x+3][threadIdx.y+1] = im[(i-1)*width + (j+1)];
+			shared_block[threadIdx.x+4][threadIdx.y+1] = im[(i-1)*width + (j+2)];
+			shared_block[threadIdx.x+3][threadIdx.y  ] = im[(i-2)*width + (j+1)];
+			shared_block[threadIdx.x+4][threadIdx.y  ] = im[(i-2)*width + (j+2)];	
+		}
+
+	}
+	
+	__syncthreads();
+	
+	float Gx, Gy; 
+	
+	if(i > 2 && i < h-2 && j >2 && j < width-2)
+	{
+		
+		Gx = 	 (1.0*shared_block[x][y]   +  2.0*shared_block[x+1][y  ] +  (-2.0)*shared_block[x+3][y  ] + (-1.0)*shared_block[x+4][y  ]
+			+ 4.0*shared_block[x][y+1] +  8.0*shared_block[x+1][y+1] +  (-8.0)*shared_block[x+3][y+1] + (-4.0)*shared_block[x+4][y+1]
+			+ 6.0*shared_block[x][y+2] + 12.0*shared_block[x+1][y+2] + (-12.0)*shared_block[x+3][y+2] + (-6.0)*shared_block[x+4][y+2]
+			+ 4.0*shared_block[x][y+3] +  8.0*shared_block[x+1][y+3] +  (-8.0)*shared_block[x+3][y+3] + (-4.0)*shared_block[x+4][y+3]
+			+ 1.0*shared_block[x][y+4] +  2.0*shared_block[x+1][y+4] +  (-2.0)*shared_block[x+3][y+4] + (-1.0)*shared_block[x+4][y+4]);
+
+		
+		Gy = 
+			((-1.0)*shared_block[x][y  ] + (-4.0)*shared_block[x+1][y  ] +  (-6.0)*shared_block[x+2][y  ] + (-4.0)*shared_block[x+3][y  ]+ (-1.0)*shared_block[x+4][y  ]
+			+(-2.0)*shared_block[x][y+1] + (-8.0)*shared_block[x+1][y+1] + (-12.0)*shared_block[x+2][y+1] + (-8.0)*shared_block[x+3][y+1]+ (-2.0)*shared_block[x+4][y+1]
+			+   2.0*shared_block[x][y+3] +    8.0*shared_block[x+1][y+3] +    12.0*shared_block[x+2][y+3] +    8.0*shared_block[x+3][y+3]+    2.0*shared_block[x+4][y+3]
+			+   1.0*shared_block[x][y+4] +    4.0*shared_block[x+1][y+4] +     6.0*shared_block[x+2][y+4] +    4.0*shared_block[x+3][y+4]+    1.0*shared_block[x+4][y+4]);
+
+
+		//Gradiente stored en im_out
+		im_out[i*width+j] = sqrtf((Gx*Gx)+(Gy*Gy));
+		
+	}
+	
+	else if (i<h && j<width) im_out[i*width+j] = 0.0;
+	
+	
+	
 }
 
 
@@ -124,7 +224,11 @@ void /*__global__*/ cannyGPU(float *im, float *image_out,
 
 	//LLamada a las funciones de tratado de imagen
 	noiseReductionGPU<<<numBlocks,threadsPerBlock>>>(im_GPU, im_out_GPU, height, width);
+		
+	cudaMemcpy(im_GPU, im_out_GPU, IMG_SIZE, cudaMemcpyDeviceToDevice);
 	
+	imageGradientGPU<<<numBlocks,threadsPerBlock>>>(im_GPU, im_out_GPU, height, width);
+
 
 	//Devolución de valores y liberado de memoria.
 	cudaMemcpy(image_out, im_out_GPU, IMG_SIZE, cudaMemcpyDeviceToHost);
